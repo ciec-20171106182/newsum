05bc2f4ba69b9c83b69694a157c7ce8381001a65
ciecS20171106331 <565738655@qq.com>
2018-10-27 08:56:59 +0800

    Initial Commit

---
+++ newsum/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  newsum
+//
+//  Created by s20171106182 on 2018/10/27.
+//  Copyright © 2018 s20171106182. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
